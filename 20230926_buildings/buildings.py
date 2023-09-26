9602c4e5ae8986ef1329c8538474c45559fd8db2
Luis Natera <luis@aerlabs.com>
2023-09-26 16:50:02 +0200

    :sparkles: Starting a new sketch!

---
+++ 20230926_buildings/buildings.py
@@ -0,0 +1,25 @@
+"""
+buildings by Luis Natera
+
+Provide some description about the script
+"""
+
+import py5
+
+
+def setup():
+    """Provides the setup for py5. It only runs once"""
+    py5.full_screen()
+
+
+def draw():
+    """
+    Drawing block, the function is run once per frame.
+    To make the function only runs once, use no_loop()
+    """
+
+    py5.no_loop()
+    py5.save_frame("buildings/images/buildings_####.png", use_thread=True)
+
+
+py5.run_sketch()
